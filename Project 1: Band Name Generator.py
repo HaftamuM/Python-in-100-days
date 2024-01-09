d0863342a3b8f937c14f5cf8b13189c5a4fbdbe3
Haftamu M <75226297+HaftamuM@users.noreply.github.com>
2024-01-09 19:46:52 +0000

    change to Name

---
+++ Project 1: Band Name Generator.py
@@ -0,0 +1,12 @@
+# The Band Name Generator is a Python program that utilizes lists, random number generation,
+# and string concatenation to create and display a unique band name by selecting random adjectives and nouns.
+
+#1 Create a greeting for your program. 
+print("Hello Dude, Welcome to a Name Generator")
+#2 Ask the user for the ciy that they grew up in. 
+City = input("Please Enter the City that you grew up in?\n")
+#3 Ask the user for the name of a pet
+Pet = input("Enter the name of your Pet?\n")
+#4 Combine the name of te their City and pet and show them their and name. 
+print("Your band Name is"" " + City + Pet)
+#5. Make sure the input cursor shows on a new line
