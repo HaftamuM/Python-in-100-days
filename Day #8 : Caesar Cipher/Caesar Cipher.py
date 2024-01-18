5668fb9a9f0325478f57225cab64f559aac1e3e2
Haftamu M <75226297+HaftamuM@users.noreply.github.com>
2024-01-18 21:08:43 +0000

    Added Day #8 project

--- Day #8 : Caesar Cipher/Caesar Cipher.py
+++ Day #8 : Caesar Cipher/Caesar Cipher.py
@@ -1,24 +1,31 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction= input(f"Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input(f"Type your message:\n")
+text = input(f"Type your message:\n").lower()
 shift = int(input("Type the shift numbers:\n"))
+ ## combine this to a 1 function when u get the time 
 
-#Todo-1 : Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs. 
+def encrypt (plain_text, shift_amount):
+    cipher_text = ""
+    for letter in plain_text: 
+        position = alphabet.index(letter)
+        new_position = position + shift_amount 
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter
+    print(f"The encoded text is {cipher_text}")
 
-if direction == 'encode':
-    def encrypt(plain_text, shift_amount):
-        cipher_text = ""
-#Todo-2 : Inside the 'encrypt' function, shift each letter of the 'text' forware in the alphabet by the shift amout 
-# and print the encypted text.   
-        for letter in plain_text: 
-            position = alphabet.index(letter)
-            new_position = position + shift_amount
-            new_letter = alphabet[new_position]
-            cipher_text += new_letter
-        print(f"The encoded text is {cipher_text}")
-    
+def decrypt (plain_text, shift_amount):
+    cipher_text = ""
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount 
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter 
+    print(f"The decoded text is {cipher_text}")
 
-#Todo-3 :call the encrpt function and pass in the user inputs. you should be able to test the code and encrypt a message 
-        encrypt(plain_text = text, shift_amount = shift)
+if direction == 'encode'.lower(): 
+    encrypt (plain_text = text,shift_amount = shift)
+elif direction == "decode".lower():
+    decrypt (plain_text = text,shift_amount = shift)
+
