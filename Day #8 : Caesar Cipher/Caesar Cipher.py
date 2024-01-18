b2d7d22311a296a2a983bf4424205a6c731f2eb8
Haftamu M <75226297+HaftamuM@users.noreply.github.com>
2024-01-18 22:08:44 +0000

    change of Day #8

--- Day #8 : Caesar Cipher/Caesar Cipher.py
+++ Day #8 : Caesar Cipher/Caesar Cipher.py
@@ -1,9 +1,13 @@
 from art import alphabet 
+from logo import Caesar
+
+print(Caesar)
 
 direction= input(f"Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input(f"Type your message:\n").lower()
 shift = int(input("Type the shift numbers:\n"))
  ## combine this to a 1 function when u get the time 
+
 
 def encrypt (plain_text, shift_amount):
     cipher_text = ""
