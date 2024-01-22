8b696b949711e0f1da61a544979468265f567eb6
Haftamu M <75226297+HaftamuM@users.noreply.github.com>
2024-01-22 19:10:35 +0000

    change of Day

--- Day #11 : Black Jack game/main.py
+++ Day #11 : Black Jack game/main.py
@@ -9,28 +9,4 @@ CLUBS    = chr(9827) # Character 9827 is '♣'.
 # (A list of chr codes is at https://inventwithpython.com/charactermap)
 BACKSIDE = 'backside'
 
-
-
-def deal_card():
-# Deal both user and computer a starting hand 2 random card 
-    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-    card = random.choice(cards)
-    return card 
-
-user_cards = [] 
-computer_cards = [] 
-
-for _ in range(3):  
-    user_cards.append(deal_card())
-    computer_cards.append(deal_card())
-
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
-
-def calculate_score(cards): 
-    if sum(cards) == 21 and len(cards) == 2:
-        return 0
-    if 11 in cards and sum(cards) > 21: 
-        cards.remove(11)
-        cards.append(1)
-    return sum(cards)
+# Redo the videos 
---
+++ Day 42 : CSS/READMe.md
@@ -0,0 +1,37 @@
+## Types of CSS
+
+## - 1 lnline Css <html style = "background: color">
+## - 2 Internal Css
+<html> 
+    <head> 
+        <style> 
+        html { 
+            background: red;
+        }
+        </style> 
+    </head>
+<html> 
+
+## - 3 External : 
+Create - styles.css
+<html> 
+    <head>
+    <link
+       <link rel="stylesheet">
+       <href ="./styles.css"/>
+    </head>
+<html> 
+------------------------------
+Types of selector : element selector 
+h2 { 
+    color:red; 
+
+}
+class selector or id (very unique)
+.red-heding { 
+    color:red
+}
+and 
+<h2 class="red-heding">
+css 
+#id { } 
