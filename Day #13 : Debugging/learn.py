c288c60e38508f3461dffdcdbfa5d0b6dddf4ec3
Haftamu M <75226297+HaftamuM@users.noreply.github.com>
2024-02-22 20:04:39 +0000

    Change of Day 13

--- Day #13 : Debugging/learn.py
+++ Day #13 : Debugging/learn.py
@@ -15,9 +15,38 @@
 # print(dice_imgs[dice_num])
 
 # code 3 - logic error ( solution: read the line one by one)
-year = int(input("What's your year of birth "))
-if year > 1989 and year < 1994:
-      print("You are a millenial.")
-elif year > 1984: 
-      print("you are a Gen z")
+# ear = int(input("What's your year of birth "))
+#if year > 1989 and year < 1994:
+#      print("You are a millenial.")
+# elif year > 1984: 
+#     print("you are a Gen z")
+
+# code 4 - Fix the errors 
+# age = int(input("How old are you?"))
+# if age > 18: 
+#  print(f"You can drive at age {age}.")
+
+
+# code 5 - Use a debugger 
+# def mutate(a_list):
+  #   b_list = [] 
+  #  for item in a_list: 
+   #      new_item = item * 2 
+   #  b_list.append(new_item)
+   #  print(b_list)
+#  mutate (1,2,3,4,5,5)
+
+# summary of code debugger 
+# 1. Describe the problem 
+# 2. Repriduce the bug
+# 3. play computer 
+# 4. Fix the error 
+# 5. Print is your friend
+# 6. Use a debugger 
+# 7. Take a break 
+# 8. Ask a friend
+# 9. Run the code often
+#10. Stackflow : Ask a developer 
+
+# even or odd number 
 
