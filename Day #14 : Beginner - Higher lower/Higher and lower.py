7c06bd53cc9ad715626230bafeacb786820d083a
Haftamu M <75226297+HaftamuM@users.noreply.github.com>
2024-02-22 22:11:14 +0000

    Change in Day 14

--- Day #14 : Beginner - Higher lower/Higher and lower.py
+++ Day #14 : Beginner - Higher lower/Higher and lower.py
@@ -1,2 +1,27 @@
-# A game that compares two values and the user guess which is the correct answer 
+from art import logo
+from game_data import data 
+import random 
+
+# Display art  from art.py 
+
+print(logo)
+# Generate a radom account from the game data. 
+
+#Format the account data into printable 
+
+#Ask the user for a guess
+
+#Chack if a user is correct 
+##Get folower count of each account. 
+## Use if statement to check if user is correct. 
+
+#Give use feeback on their guess... tracking score 
+
+#Score keeping. 
+
+#Make the game repeatble.
+
+#Making account at posiition B become the next account at Posision A
+
+#CLear the screen betwen round 
 
