9249a097756b022e3bd463033f988d5002f5c1af
Haftamu M <75226297+HaftamuM@users.noreply.github.com>
2024-02-26 19:19:40 +0000

    Change to Day 10-19

---
+++ Day 10 - 19 : Basic Python II/Day #15 : Coffee Machine Program/main.py
@@ -0,0 +1,36 @@
+# program requiremnt - follow the coffee machine 
+# TODO: 1. print report
+# TODO: 2. check resources sufficient? 
+# TODO: 3. Process using a cone 
+# TODO: 4. Check tranaction sucessful?
+# TODO: 5.Make the coffe
+
+MENU = { 
+    "espresso": {
+        "ingredients":{
+        "water":50,
+        "coffee":18,
+     },
+     "cost": 1.5,
+},
+    "Latte": {
+        "ingredients":{
+        "water":200,
+        "milk":150,
+        "coffee":18,
+     },
+     "cost": 1.5,
+},
+    "caooaccino": {
+        "ingredients":{
+        "water":200,
+        "milk":150,
+        "coffee":18,
+     },
+     "cost": 3.0,
+}}
+resources = { 
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
