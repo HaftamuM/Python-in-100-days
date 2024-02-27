d8113d892e44bba4c4798733e850797f5dfef0a9
Haftamu M <75226297+HaftamuM@users.noreply.github.com>
2024-02-27 17:12:05 +0000

    Change to Day 16

---
+++ Day 10 - 19 : Basic Python II/Day #16 : Coffee Machine Program OOP/main.py
@@ -0,0 +1,9 @@
+#TODO1 Print report 
+#TODO2 Check Resource Sufficent 
+#TODO3 Process coins 
+#TODO4 Check transaction sucessful
+#TODO5 Make coffee
+
+from menu import menu, menuITem 
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
