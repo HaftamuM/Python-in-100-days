276d4eb2d8f040d635fe7377b4c1083e7b0bf708
Haftamu M <75226297+HaftamuM@users.noreply.github.com>
2024-03-02 17:30:43 +0000

    change to Day 18

---
+++ Day 10 - 19 : Basic Python II/Day #18 : Turtle and GUI/Learning/main.py
@@ -0,0 +1,77 @@
+import turtle as t
+import random 
+
+# timmy_the_turtle = Turtle()
+# timmy_the_turtle.shape ("turtle") 
+# timmy_the_turtle.color ("Red")
+
+# for _ in range (4):
+   # timmy_the_turtle.forward(180)
+   # timmy_the_turtle.lef(90)
+
+
+#Draw a dash line 
+
+#tim = t.Turtle()
+
+#def draw_shape(num_sides):
+    #angel = 360/ num_sides 
+   # for _ in range(15):
+      #  tim.forward(10)
+       # tim.pen()
+       # tim.forward(10)
+       # tim.pendown()
+
+# Draw a random walk using rand.pensize, color
+
+#tim  = t.Turtle()
+
+#colours = ["cornflowerBlue", "DarkOrchid", "IndianRed"]
+#directions =  [9, 90, 180, 270]
+#tim.pensize(15)
+#tim.speed("fastest")
+
+#for _ in range(200):
+    #tim.color(random.choice(colours))
+   # tim.forward(30)
+   # tim.setheading(random.choice(directions))
+
+#create a random color using Tuples
+#(1,2,3) - alist for a color - stays constant
+    
+# def random_color(): 
+    #r = random.randin(0, 255)
+    #g = random.randin(0, 255)
+    #b = random.randin(0, 255)
+    #random_color = (r,g,b)
+
+#tim.color(random_color)
+
+##Create a spiral using the library 
+
+#tim.speed("fastest")
+
+#def draw_spirograph(size_of_gap):
+    #for _ in range(int(360 / size_of_gap)): # 100 rotation    
+       # tim.color(random_color())
+       # tim.circle(100)
+      #  tim.setheading(tim.heading() + 10)
+        
+#draw_spirograph(5)
+
+#screen = t.Screen() 
+#screen.exitonclick() 
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick(
+)
---
+++ Day 10 - 19 : Basic Python II/Day #18 : Turtle and GUI/hirst-painting/main.py
@@ -0,0 +1,7 @@
+#install the package cologram 
+
+import cologram
+
+#1 extract method
+colors = cologram.extract('image.jpg': 30)
+print(colors)
