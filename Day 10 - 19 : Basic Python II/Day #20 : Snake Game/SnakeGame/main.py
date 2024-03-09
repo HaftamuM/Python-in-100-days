d40e131a13a25d8306714027403c961e9ffe511c
Haftamu M <75226297+HaftamuM@users.noreply.github.com>
2024-03-09 21:51:49 +0000

    change of Day 20

--- Day 10 - 19 : Basic Python II/Day #20 : Snake Game/SnakeGame/main.py
+++ Day 10 - 19 : Basic Python II/Day #20 : Snake Game/SnakeGame/main.py
@@ -1,4 +1,10 @@
+#create a snake class
+#create a food
+#create a scoreboard 
+
 from turtle import screen, Turtle
+from snake import Snake
+import time 
 
 screen = screen()
 screen.setup(width=600, height=600)
@@ -20,17 +26,16 @@ screen.tracer(0)
 #segment_3.goto(-40, 0)
 
 # Create a snake body 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)] # how tuples works 
+
 
 segments =  []
-for position in starting_positions: 
-    new_segment = Turtle("Square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+
 
 # Move the snake forward with a given time, and speed
+
+snake = Snake()
+
+
 game_is_on = True 
 while game_is_on: 
     screen.updat()
@@ -39,11 +44,6 @@ while game_is_on:
       #  seg.forwared(20)
 #Turn the segments in different direction , moving the third segment to the second, and the second to the 
 #first segment 
-    
-    for seg_num in range(len(segments)-1 , 0 , -1):
-        new_x = segments[seg_num-1].xcor()
-        new_y = segments[seg_num -1].ycor()
-        segments[seg_num].goto(new_x, new_y) 
-    segments[0].forward(20)
+    snake.move()
 
 screen.exitonclick()
---
+++ Day 10 - 19 : Basic Python II/Day #20 : Snake Game/SnakeGame/snake.py
@@ -0,0 +1,21 @@
+from turtle import turtle
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)] # how tuples works 
+
+class Snake: 
+    def __init__(self): #intalizaing the class 
+        self.segments = [] 
+        self.create_snake()
+    def create_snake(self):
+        for position in STARTING_POSITIONS: 
+            new_segment = Turtle("Square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+    def mov(self):
+        for seg_num in range(len(self.segments)-1 , 0 , -1):
+            new_x = self.segments[seg_num-1].xcor()
+            new_y = self.segments[seg_num -1].ycor()
+            self.segments[seg_num].goto(new_x, new_y) 
+        self.segments[0].forward(20)
