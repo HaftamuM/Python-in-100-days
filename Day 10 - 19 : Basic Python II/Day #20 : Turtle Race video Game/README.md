7185c4bb54d6261bc1a145a25cc82c8cd097d1ab
Haftamu M <75226297+HaftamuM@users.noreply.github.com>
2024-03-09 20:42:08 +0000

    Change of Day 19

--- Day 10 - 19 : Basic Python II/Day #20 : Turtle Race video Game/README.md
+++ Day 10 - 19 : Basic Python II/Day #20 : Turtle Race video Game/README.md
@@ -7,3 +7,7 @@
 Project 1 : Band Name Generator
 
 "Turbo Turtles: Racing Rally" is a vibrant and engaging racing game where players take on the role of adorable turtles competing on colorful tracks filled with obstacles and power-ups. With accessible yet deep gameplay mechanics, players drift, boost, and navigate through challenging circuits, aiming to conquer the championship. The game features a diverse roster of turtles, each with unique abilities, and offers various game modes, including a thrilling single-player campaign and competitive multiplayer battles. With its charming visuals and addictive gameplay, "Turbo Turtles" promises an exhilarating racing experience that players of all ages can enjoy.
+
+Step 1 - in playing the game .. who is going to win the game 
+Step 2 - and your press play or space to start the race... 
+Step 3 - you will get the result 
--- Day 10 - 19 : Basic Python II/Day #20 : Turtle Race video Game/main.py
+++ Day 10 - 19 : Basic Python II/Day #20 : Turtle Race video Game/main.py
@@ -1,34 +1,76 @@
 #https://www.freecodecamp.org/news/how-to-make-racing-game-using-python/
 #Add the pictures and more 
-
+# creating differnet object from a class.. which a class is variable
+#timmy = Turtle() - instance 
+#tommy = Turtle() - instance 
+#The state of different  objects .. are different 
 
 from turtle import Turtle, screen 
 
 time = Turtle()
 screen = Screen()
 
-def mov_forwards():
-    tim.forward(10)
+#def mov_forwards():
+#    tim.forward(10)
 
-def mov_backwards():
-    tim.forward(10)
+#def mov_backwards():
+#    tim.forward(10)
 
-def turn_left():
-    new_heading = tim.heading() + 10
-    tim.setheading(new_heading)
+#def turn_left():
+  #  new_heading = tim.heading() + 10
+ #   tim.setheading(new_heading)
 
-def turn_right():
-    new_heading = tim.heading() - 10
-    tim.setheading(new_heading)
+#def turn_right():
+    #new_heading = tim.heading() - 10
+   # tim.setheading(new_heading)
 
-def clea() 
+#def clear(): 
+    #tim.clear()
+   # tim.penup()
+  #  tim.home()
+ #   tim.pendown()
+
+
+#screen.listen()
+#screen.onkey(move_forwards, "w")
+#screen.onkey(move_backwards, "s")
+#screen.onkey(move_left, "a")
+#screen.onkey(turn_right, "d")
+#screen.onkey(clear, "c")
+screen.set(widt=500, height=400 )
+user_bet = screen.textinput(title="Make your Bet", prompt="Which turtle will win the reace? Enter a color:")
+colors = ["red", "orange","yellow", "green", "blue", "blue", "Purple"]
+all_turtles = []
+
+
+y_positions = [-70, -40, -10, 20, 50,80]
+
+for turtle_index in range(0,6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[turtle_index_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    all_turtles.append(new_turtle)
+#ramdom moveing 
     
-screen.listen()
-screen.onkey(move_forwards, "w")
-screen.onkey(move_backwards, "s")
-screen.onkey(move_left, "a")
-screen.onkey(turn_right, "d")
-screen.onkey(clear, "c")
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+
+    for turtle in all_turtles:
+        if turtle.xcor()  > 230: 
+            is_race_on = False 
+            wining_color = turtle.pencolor()
+            if wining_color == user_bet: 
+                print(f"You have won! The {winning_color} turtle is the wineer!")
+            else: 
+                print(f"You have lost! The {winning_color} turtle is the winner!")
+            
+
+
+        rand_distance = random.randin(0, 10)
+        turtle.forward(rand_distnace)
 
 screen.exitonclick()
 
